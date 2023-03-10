3c7d2d1799be4af85f263130ef21ba034e6872cb
divyanshagarwal053 <81041580+divyanshagarwal053@users.noreply.github.com>
2023-03-11 00:16:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+var typed = new Typed(".animated-text", {
+    strings: ["Data Scientists", "App Developer", "Singer"], typeSpeed: 100,
+    backSpeed: 100,
+    backdelay: 1000,
+    loop: true
+})
+
+/*-------about-----*/
+
+var tablinks = document.getElementsByClassName("tab_links")
+var tabcontents = document.getElementsByClassName("tab_contents")
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active_link")
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active_tab")
+    }
+    event.currentTarget.classList.add("active_link")
+    document.getElementById(tabname).classList.add("active_tab")
+}
+
+
+
+/*-------project load more------*/
+// let loadMoreBtn = document.querySelector('#load_more');
+// let currentItem = 3;
+
+// loadMoreBtn.onclick = () => {
+//     let boxes = [...document.querySelectorAll(".project")];
+//     for (var i = currentItem; i < currentItem + 3; i++) {
+//         boxes[i].style.display = 'inline-block';
+//     }
+//     currentItem += 3;
+// }
+
+
+var sidemeu = document.getElementById("sidemenu")
+
+function openmenu() {
+    sidemeu.style.right = "0"
+}
+function closemenu() {
+    sidemeu.style.right = "-200px"
+}
