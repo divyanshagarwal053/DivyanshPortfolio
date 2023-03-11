811e90359d1a0ca1b6d58d5c7ca35bb3d5765cd5
divyanshagarwal053 <81041580+divyanshagarwal053@users.noreply.github.com>
2023-03-11 18:50:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,33 +8,33 @@ var typed = new Typed(".animated-text", {
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-// window.onscroll = () => {
-//     sections.forEach(sec => {
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop - 150;
-//         let height = sec.offsetHeight;
-//         let id = sec.getAttribute('id');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-//         if(top>=offset && top < offset + height){
-//             navLinks.forEach(links => {
-//                 links.classList.remove('active');
-//                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-//             });
-//         };
-//     });
+        if(top>=offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
 
-//     /*-----------------sticky navbar---------------*/
-//     let header = document.querySelector('header');
+    /*-----------------sticky navbar---------------*/
+    let header = document.querySelector('header');
 
-//     header.classList.toggle('sticky', window.scrollY > 100);
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-//     /*-------------remove toggle icon and navbar----------------------*/
-//     menuIcon.classList.remove('fa-xmark');
-//     navbar.classList.remove('active');
+    /*-------------remove toggle icon and navbar----------------------*/
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
 
 
 
-// };
+};
 
 /*-------about-----*/
 
@@ -62,11 +62,11 @@ menuIcon.onclick = () => {
 }
 
 
-var sidemeu = document.getElementById("sidemenu")
+// var sidemeu = document.getElementById("sidemenu")
 
-function openmenu() {
-    sidemeu.style.right = "0"
-}
-function closemenu() {
-    sidemeu.style.right = "-200px"
-}
+// function openmenu() {
+//     sidemeu.style.right = "0"
+// }
+// function closemenu() {
+//     sidemeu.style.right = "-200px"
+// }
