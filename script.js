70395ee41388150ceba09730ae175dad6302c12d
divyanshagarwal053 <81041580+divyanshagarwal053@users.noreply.github.com>
2023-03-11 18:27:21 +0530

    after changing website

--- script.js
+++ script.js
@@ -5,6 +5,37 @@ var typed = new Typed(".animated-text", {
     loop: true
 })
 
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top>=offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    /*-----------------sticky navbar---------------*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*-------------remove toggle icon and navbar----------------------*/
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+
+
+
+};
+
 /*-------about-----*/
 
 var tablinks = document.getElementsByClassName("tab_links")
@@ -22,18 +53,13 @@ function opentab(tabname) {
 }
 
 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-/*-------project load more------*/
-// let loadMoreBtn = document.querySelector('#load_more');
-// let currentItem = 3;
-
-// loadMoreBtn.onclick = () => {
-//     let boxes = [...document.querySelectorAll(".project")];
-//     for (var i = currentItem; i < currentItem + 3; i++) {
-//         boxes[i].style.display = 'inline-block';
-//     }
-//     currentItem += 3;
-// }
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
 
 
 var sidemeu = document.getElementById("sidemenu")
